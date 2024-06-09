df938248ba3a5e70d2963265043e391a1fb1e996
Godwin <godwinchuks032@gmail.com>
2024-06-09 15:26:11 +0100

    function floor that return floor float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module typea-annotation floor"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """take n argunment and return the floor of float"""
+    return math.floor(n)
