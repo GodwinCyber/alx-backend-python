f00ae2d6ade3562c69d384d11fadb65e1a3af73e
Godwin <godwinchuks032@gmail.com>
2024-06-09 21:53:14 +0100

    type-annotation for multiply the multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Module 8"""
 
+from typing import Callable
 
-def make_multiplier(multiplier: float) -> float:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Make a function that multiplies by multiplier"""
-    def multiplier_function(x: float) -> float:
+    def multiplier_function(value: float) -> float:
         """Multiply by multiplier"""
-        return x * multiplier
+        return value * multiplier
     return multiplier_function
