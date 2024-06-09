ff188f3d44733ea0e805f962ebbc4ad75b28e773
Godwin <godwinchuks032@gmail.com>
2024-06-09 16:30:14 +0100

    sum function type annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module 5 take input list, return float"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum_list function type annotations"""
+    return sum(input_list)
