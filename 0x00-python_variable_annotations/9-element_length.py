63046291f773fd30cce4faa8eb292cc827b8a950
Godwin <godwinchuks032@gmail.com>
2024-06-10 03:26:43 +0100

    Return Iterable Object

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module 9"""
+
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return an Iterable object"""
+    return [(i, len(i)) for i in lst]
