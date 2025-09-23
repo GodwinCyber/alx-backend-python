aaaba4fca675550033ab65ef93ca0c5c86308d7c
GodwinCyber <godwinchuks032@gmail.com>
2025-09-23 18:58:27 +0100

    An endpoint that uses viewSets from rest_framework to list conversations, create a conversation. Create a message, and display an existing message

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
 # Create your views here.
+# Using viewsets from rest-framework Create viewsets for listing conversations (ConversationViewSet) and messages (MessageViewSet)
+# Implement the endpoints to create a new conversation and send messages to an existing one
+
+class ConersationViewSet(viewsets.ModelViewSet):
+    '''ViewSet for listing and creating conversations'''
+    queryset = Conversation.objects.all() # Fetch all conversations
+    serializer_class = ConversationSerializer # Use ConversationSerializer for serialization
+    http_method_names = ['get', 'post'] # Allow only GET and POST methods
+    filterset_fields = ['participants'] # Allow filtering by participants
+    search_fields = ['title'] # Allow searching by title
+    ordering_fields = ['created_at'] # Allow ordering by creation date
+    ordering = ['-created_at'] # Default ordering by creation date descending
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    '''ViewSet for listing and creating messages'''
+    queryset = Message.objects.all() # Fetch all messages
+    serializer_class = MessageSerializer # Use MessageSerializer for serialization
+    http_method_names = ['get', 'post'] # Allow only GET and POST methods
+    filterset_fields = ['conversation', 'sender'] # Allow filtering by conversation and sender
+    search_fields = ['content'] # Allow searching by content
+    ordering_fields = ['created_at'] # Allow ordering by creation date
+    ordering = ['-created_at'] # Default ordering by creation date descending
+
