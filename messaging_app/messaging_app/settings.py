ede0571c09a46f843307016ce0c398cf910f6e9d
GodwinCyber <godwinchuks032@gmail.com>
2025-09-25 13:12:27 +0100

    Creating the authentication in the setting, added auth to return the jwt token given as a user, url for the authentication endpoint

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.athentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
